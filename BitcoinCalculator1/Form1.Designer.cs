39ff73e8df7cf3443c404ee63afc7a8cc95d43a3
Emil Rand <Emil.Randit@gmail.com>
2022-02-16 21:23:26 +0200

    first commit

---
+++ BitcoinCalculator1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace BitCoinCalculato
+{
+    partial class BitcoinCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.currencyMenu = new System.Windows.Forms.ComboBox();
+            this.getRates = new System.Windows.Forms.Button();
+            this.amountOfBtc = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // currencyMenu
+            // 
+            this.currencyMenu.FormattingEnabled = true;
+            this.currencyMenu.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.currencyMenu.Location = new System.Drawing.Point(50, 33);
+            this.currencyMenu.Name = "currencyMenu";
+            this.currencyMenu.Size = new System.Drawing.Size(104, 21);
+            this.currencyMenu.TabIndex = 0;
+            this.currencyMenu.Text = "Select currency";
+            // 
+            // getRates
+            // 
+            this.getRates.Location = new System.Drawing.Point(171, 96);
+            this.getRates.Name = "getRates";
+            this.getRates.Size = new System.Drawing.Size(64, 20);
+            this.getRates.TabIndex = 1;
+            this.getRates.Text = "Get Rates";
+            this.getRates.UseVisualStyleBackColor = true;
+            this.getRates.Click += new System.EventHandler(this.getRates_Click);
+            // 
+            // amountOfBtc
+            // 
+            this.amountOfBtc.Location = new System.Drawing.Point(50, 96);
+            this.amountOfBtc.Name = "amountOfBtc";
+            this.amountOfBtc.Size = new System.Drawing.Size(104, 20);
+            this.amountOfBtc.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(50, 173);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(104, 20);
+            this.result.TabIndex = 3;
+            this.result.Visible = false;
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(50, 148);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(37, 13);
+            this.resultLbl.TabIndex = 4;
+            this.resultLbl.Text = "Result";
+            this.resultLbl.Visible = false;
+            // 
+            // BitcoinCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.amountOfBtc);
+            this.Controls.Add(this.getRates);
+            this.Controls.Add(this.currencyMenu);
+            this.Name = "BitcoinCalculator";
+            this.Text = "Bitcoin Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currencyMenu;
+        private System.Windows.Forms.Button getRates;
+        private System.Windows.Forms.TextBox amountOfBtc;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label resultLbl;
+
+        public EventHandler currencyMenu_SelectedIndexChanged { get; private set; }
+    }
+}
